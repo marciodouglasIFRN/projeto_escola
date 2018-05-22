2bacf0120bde30f15de0f61eb2839bf844e4c2c5
Adelson111 <adelson.oliveira111@gmail.com>
2018-05-22 09:48:16 -0300

    inicio

---
+++ page/view/login.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>Escola</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../includ/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/size.css">
+		<link rel="stylesheet" type="text/css" href="../css/color.css">
+	</head>
+	<body>
+		<div class="form-login">
+			<form action="/action_page.php">
+			  <div class="form-group">
+			    <label for="email">Matricula</label>
+			    <input type="email" class="form-control" id="email">
+			  </div>
+			  <div class="form-group">
+			    <label for="pwd">Senha</label>
+			    <input type="password" class="form-control" id="pwd">
+			  </div>
+			  <div class="form-group form-check">
+			    <label class="form-check-label">
+			      <input class="form-check-input" type="checkbox"> Lembrar-me
+			    </label>
+			  </div>
+			  <button type="submit" class="btn btn-primary">Entrar</button>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ page/view/secretario/inicio.php
@@ -0,0 +1,12 @@
+<<html>
+	<head>
+		<title>Início</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../../includ/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../../css/size.css">
+		<link rel="stylesheet" type="text/css" href="../../css/color.css">
+	</head>
+	<body>
+		
+	</body>
+</html>
